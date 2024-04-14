bfc13ddfda12fdf57be17c1ab7cc85171b240b45
mrdev1020@gmail.com <mrdev1020@gmail.com>
2024-04-14 15:10:45 +0330

    feat: product service

--- ng-video-game-db/src/app/pages/admin/products/products.component.ts
+++ ng-video-game-db/src/app/pages/admin/products/products.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgbAlertModule } from '@ng-bootstrap/ng-bootstrap';
+import { ProductService } from '../../services/product/product.service';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule, NgbAlertModule],
   templateUrl: './products.component.html',
-  styleUrl: './products.component.scss'
+  styleUrl: './products.component.scss',
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
+  isSidePanelVisible: boolean = true;
+  product: any = {
+    id: 0,
+    name: '',
+  };
+  categoryList: any[] = [];
 
+  constructor(private productSrv: ProductService) {}
+
+  ngOnInit(): void {
+    this.getAllCategory();
+  }
+
+  getAllCategory() {
+    this.productSrv.getAllProducts().subscribe((res: any) => {
+      this.categoryList = res.data;
+    });
+  }
+
+  toggleAlert() {
+    this.isSidePanelVisible = !this.isSidePanelVisible;
+  }
 }
---
+++ ng-video-game-db/src/app/pages/services/constant/constant.ts
@@ -0,0 +1,7 @@
+export const Constant = {
+    API_END_POINT: 'https://jsonplaceholder.typicode.com/',
+    METHODS: {
+        GET_ALL_PRODUCT: 'albums',
+        GET_ALL_CATEGORY: 'users'
+    }
+}
